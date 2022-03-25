de3e9c256546eb8873589ae3095f405ad49708fc
claudemirslopes <claudemir.slopes@hotmail.com>
2022-03-25 14:33:18 -0300

    Novas Alterações

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 $url_host = filter_input(INPUT_SERVER, 'HTTP_HOST');
 // AQUI NO LOCALHOST
-define('pg', "http://$url_host/SisFestas");
+define('pg', "http://$url_host/public/SisFestas");
 // AQUI NO SERVIDOR DE HOSPEDAGEM
 // define('pg', "https://vieiraeguimaraesadvocacia.com.br/site/adm");
